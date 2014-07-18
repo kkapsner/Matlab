71ee4f770689b370cb3d396971e5ca0ca5c30dc1
kkapsner <github@mail.kkapsner.de>
2014-07-19 00:34:49 +0200

    Filter.gauss cleanup

--- functions/+Filter/gauss.m
+++ functions/+Filter/gauss.m
@@ -1,16 +1,24 @@
 function filtered = gauss(data, sigma, dim)
 %GAUSS performs a filter with a symmetric gaussian filter
-%   The data is filtered by a weighted moving average. The weighting
-%   function is a gaussian with width sigma.
+%   The data is filtered by a weighted moving average. The
+%   weighting function is a gaussian with width sigma.
 %   
-%   FILTERED = gauss(DATA)              filters the DATA with SIGMA = 3
-%   FILTERED = gauss(DATA, SIGMA)       filters the DATA with SIGMA
-%   FILTERED = gauss(DATA, SIGMA, DIM)  filters along dimension DIM
+%   FILTERED = gauss(DATA)
+%       filters the DATA with SIGMA = 3
+%   FILTERED = gauss(DATA, SIGMA)
+%       filters the DATA with SIGMA
+%   FILTERED = gauss(DATA, SIGMA, DIM)
+%       filters along dimension DIM
+
     if (nargin < 2)
+        % default value for sigma
         sigma = 3;
     end
     
+    
     if (nargin < 3)
+        % no DIM argument provided -> shift the matrix to the
+        % first non singular dimension
         [data, nshifts] = shiftdim(data);
         
         filtered = shiftdim( ...
@@ -32,19 +40,22 @@ function filtered = gauss(data, sigma, dim)
     end 
 end
 
-    function filtered = performGauss(data, sigma)
-        if (sigma == 0)
-            filtered = data;
-        else
-            threeSigma = ceil(3*sigma);
-            g = exp(-(-threeSigma:threeSigma).^2/2/sigma^2);
-            
-            filtered = zeros(size(data));
-            for i = 1:size(data, 2)
-                filtered(:, i) = conv(data(:, i), g, 'same') ./ ...
-                    conv(ones(size(data(:, i))), g, 'same');
-            end
+function filtered = performGauss(data, sigma)
+% the actual filter function
+    if (sigma == 0)
+        % no filtering necessary
+        filtered = data;
+    else
+        threeSigma = ceil(3*sigma);
+        g = exp(-(-threeSigma:threeSigma).^2/2/sigma^2);
+
+        filtered = zeros(size(data));
+        for i = 1:size(data, 2)
+            % perform the convolution for every column
+            filtered(:, i) = conv(data(:, i), g, 'same') ./ ...
+                conv(ones(size(data(:, i))), g, 'same');
         end
-        
     end
+
+end
 
--- functions/+Filter/gauss2D.m
+++ functions/+Filter/gauss2D.m
@@ -1,24 +1,32 @@
 function filtered = gauss2D(data, sigma, dim)
-%GAUSS2D performs a filter with a symmetric gaussian filter in 2D
-%   The data is filtered by a weighted moving average. The weighting
-%   function is a  two dimensonal gaussian with width sigma.
+%GAUSS2D performs a filter with a symmetric gaussian in 2D
+%   The data is filtered by a weighted moving average.
+%   The weighting function is a  two dimensonal gaussian
+%   with width sigma.
 %   
-%   FILTERED = gauss2D(DATA)              filters the DATA with SIGMA = 3
-%   FILTERED = gauss2D(DATA, SIGMA)       filters the DATA with SIGMA
-%   FILTERED = gauss2D(DATA, SIGMA, DIM)  filters along dimension DIM
+%   FILTERED = gauss2D(DATA)
+%       filters the DATA with SIGMA = 3
+%   FILTERED = gauss2D(DATA, SIGMA)
+%       filters the DATA with SIGMA
+%   FILTERED = gauss2D(DATA, SIGMA, DIM)
+%       filters along dimension DIM
 %
 %   SEE ALSO: Filter.gauss
 
-    if (nargin < 2)
+    if (nargin < 2 || numel(sigma) < 1)
+        % default value for sigma
         sigma = [3 3];
-    elseif numel(sigma) < 1
     elseif numel(sigma) < 2
+        % if only one sigma is provided
         sigma = [sigma sigma];
     elseif numel(sigma) > 2
+        % if too many sigmas are provided
         sigma = sigma(1:2);
     end
     
     if (nargin < 3)
+        % no DIM argument provided -> shift the matrix to the
+        % first non singular dimension
         [data, nshifts] = shiftdim(data);
         
         filtered = shiftdim( ...
@@ -44,15 +52,18 @@ function filtered = gauss2D(data, sigma, dim)
     end 
 
     function filtered = performGauss(data, sigma)
+    % the actual filter function
         if (all(sigma == 0))
+            % no filtering necessary
             filtered = data;
         else
             threeSigma = ceil(3*sigma);
-            g1 = exp(-(-threeSigma(1):threeSigma(1)).^2/sigma(1)^2);
-            g2 = exp(-(-threeSigma(2):threeSigma(2)).^2/sigma(2)^2);
+            g1 = exp(-(-threeSigma(1):threeSigma(1)).^2/2/sigma(1)^2);
+            g2 = exp(-(-threeSigma(2):threeSigma(2)).^2/2/sigma(2)^2);
+            
+            % perform the 2D convolution
             filtered = conv2(g1, g2, data, 'same') ./ conv2(g1, g2, ones(size(data)), 'same');
         end
-        
     end
 end
 
