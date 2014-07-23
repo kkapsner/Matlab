21459b111f009e89ae8111117b6850f6cd66a56d
kkapsner <github@mail.kkapsner.de>
2014-07-23 23:36:28 +0200

    detectSteps cleanup

--- functions/detectSteps.m
+++ functions/detectSteps.m
@@ -1,15 +1,16 @@
 function steps = detectSteps(data, varargin)
-%DETECTSTEPS
+%DETECTSTEPS performs a step detection
 %
-% STEPS = detectSteps(DATA) detects the steps in the DATA vector and stores
-% the positions of the steps in the vector STEPS.
+% STEPS = detectSteps(DATA)
+%   detects the steps in the DATA vector and stores the
+%   positions of the steps in the vector STEPS.
 % STEPS = detectSteps(..., paramKey, paramValue)
 %
 % Parameter:
-%           medianLength  (3)    : the median filter half window size
-%           comparePoint  (0.9)  : the compare quantile
-%           compareFactor (5)    : the compare factor
-%           debug         (false): flag for debug mode
+%   medianLength  (3)    : the median filter half window size
+%   comparePoint  (0.9)  : the compare quantile
+%   compareFactor (5)    : the compare factor
+%   debug         (false): flag for debug mode
 
     %% get input parameter
     p = inputParser;
@@ -21,10 +22,11 @@ function steps = detectSteps(data, varargin)
     
     % the filter length
     medianLength = p.Results.medianLength;
-    % the point in the population to compare with (i.e. the quantile)
+    % the point in the population to compare with
+    % (i.e. the quantile)
     comparePoint = p.Results.comparePoint;
-    % the factor by which the compare point has to be mulipied to get the
-    % threshold
+    % the factor by which the compare point has to be mulipied
+    % to get the threshold
     compareFactor = p.Results.compareFactor;
     
     %% data too short
@@ -34,16 +36,23 @@ function steps = detectSteps(data, varargin)
     end
     
     %% validate data input
-    assert(isvector(data), 'detectSteps:dataNoVector', 'Data has to be a vector.');
+    assert( ...
+        isvector(data), ...
+        'detectSteps:dataNoVector', ...
+        'Data has to be a vector.' ...
+    );
     
     %% filter data, calculate derivative and generate threshold
     filteredData = medfilt1(data, 2*medianLength + 1);
     dFilteredData = abs(diff(filteredData));
     
     % remove artefacts from median filtering
-    trimmedDFilteredData = dFilteredData(medianLength:(end-medianLength));
+    trimmedDFilteredData = ...
+        dFilteredData(medianLength:(end-medianLength));
     % calculate threshold
-    compareValue = quantile(trimmedDFilteredData, comparePoint) * compareFactor;
+    compareValue = ...
+        quantile(trimmedDFilteredData, comparePoint) * ...
+        compareFactor;
     % get step positions
     steps = find(trimmedDFilteredData > compareValue) ...
         + medianLength - 1;
