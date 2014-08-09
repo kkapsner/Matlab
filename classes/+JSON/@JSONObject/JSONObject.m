e1e3beeb89990d0a7707ec00ee28920f7c64b2ae
kkapsner <github@mail.kkapsner.de>
2014-08-09 02:57:55 +0200

    Added multi level assigment to JSONObject.

--- classes/+JSON/@JSONObject/JSONObject.m
+++ classes/+JSON/@JSONObject/JSONObject.m
@@ -11,7 +11,7 @@ classdef JSONObject < containers.Map
         end
         
         function value = subsref(this, name)
-            if (numel(name) == 1 && numel(name.subs) > 1)
+            if (numel(name) == 1 && strcmp(name.type, '()') && numel(name.subs) > 1)
                 oneName = name;
                 oneName.subs = oneName.subs(1);
                 value = this.subsref@containers.Map(oneName);
@@ -21,6 +21,31 @@ classdef JSONObject < containers.Map
                 value = value.subsref(restName);
             else
                 value = this.subsref@containers.Map(name);
+            end
+        end
+        
+        function this = subsasgn(this, name, value)
+            if (numel(name) == 1 && strcmp(name.type, '()') && numel(name.subs) > 1)
+                
+                firstName = name.subs{1};
+                
+                oneName = name;
+                oneName.subs = oneName.subs(1);
+                
+                restName = name;
+                restName.subs = restName.subs(2:end);
+                if (this.isKey(firstName))
+                    newSubValue = this.subsref(oneName);
+                else 
+                    newSubValue = JSONObject();
+                end
+                newSubValue.subsasgn(restName, value);
+                this = this.subsasgn@containers.Map( ...
+                    oneName, ...
+                    newSubValue ...
+                );
+            else
+                this.subsasgn@containers.Map(name, value);
             end
         end
     end
