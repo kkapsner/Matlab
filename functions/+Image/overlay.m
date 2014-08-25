34b81737e1978f18726e14e44595535712befc31
kkapsner <github@mail.kkapsner.de>
2014-08-25 23:40:01 +0200

    Image.overlay() improvements

--- functions/+Image/overlay.m
+++ functions/+Image/overlay.m
@@ -1,14 +1,37 @@
-function image = overlay(image, overlayImage, color)
+function image = overlay(image, overlayImage, varargin)
+    p = inputParser();
+    p.addParameter('color', [1, 1, 1], @(c)isnumeric(c) && numel(c) == 3);
+    p.addParameter('opacity', 1, @(o)isnumeric(o) && isscalar(o));
+    p.addParameter('mixMode', 'mix', @ischar);
+    p.addParameter('clipping', true, @islogical);
+    p.parse(varargin{:});
+    
     if (ismatrix(image))
         image = image | overlayImage;
     else
-        if (nargin < 3)
-            color = [1, 1, 1];
-        end
-        filter = find(overlayImage);
-        image(filter + 0*size(image, 1)*size(image, 2)) = color(1);
-        image(filter + 1*size(image, 1)*size(image, 2)) = color(2);
-        image(filter + 2*size(image, 1)*size(image, 2)) = color(3);
         
+        overlayImage = overlayImage * p.Results.opacity;
+        
+        image(:, :, 1) = mix(image(:, :, 1), overlayImage, p.Results.color(1));
+        image(:, :, 2) = mix(image(:, :, 2), overlayImage, p.Results.color(2));
+        image(:, :, 3) = mix(image(:, :, 3), overlayImage, p.Results.color(3));
+
+        if (p.Results.clipping)
+            image = min(1, max(0, image));
+        end
+        
+    end
+    
+    function c = mix(c1, c2, color)
+        switch p.Results.mixMode
+            case 'mix'
+                c = (1 - c2) .* c1 + c2 .* color;
+            case 'add'
+                c = c1 + c2 .* color;
+            case 'sub'
+                c = c1 - c2 .* color;
+            otherwise
+                error('Image:overlay:unknownMixMode', 'Unknown mix mode.');
+        end
     end
 end
