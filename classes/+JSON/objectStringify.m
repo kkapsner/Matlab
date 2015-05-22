83f403732874721062c02234124088e57e187085
kkapsner <github@mail.kkapsner.de>
2015-05-22 17:35:01 +0200

    Added JSON.objectToMap

--- classes/+JSON/objectStringify.m
+++ classes/+JSON/objectStringify.m
@@ -1,12 +1,4 @@
 function json = objectStringify(obj, param, varargin)
-
-    map = containers.Map({'MATLAB::constructor'}, {class(obj)}, 'UniformValues', false);
-    
-    if (~isempty(param))
-        map('MATLAB::constructor::parameter') = param;
-    end
-    for i = 1:numel(varargin)
-        map(varargin{i}) = obj.(varargin{i});
-    end
+    map = JSON.objectToMap(obj, param, varargin{:});
     json = JSON.stringify(map);
 end
---
+++ classes/+JSON/objectToMap.m
@@ -0,0 +1,11 @@
+function map = objectToMap(obj, param, varargin)
+
+    map = containers.Map({'MATLAB::constructor'}, {class(obj)}, 'UniformValues', false);
+    
+    if (~isempty(param))
+        map('MATLAB::constructor::parameter') = param;
+    end
+    for i = 1:numel(varargin)
+        map(varargin{i}) = obj.(varargin{i});
+    end
+end
