1aaada5fd513c3b6772820e97a979c9858c08456
kkapsner <github@mail.kkapsner.de>
2015-07-08 17:04:38 +0200

    Modified Axes.fillBetween to accept traces as input.

--- PhD/+Paper/@Axes/fillBetween.m
+++ PhD/+Paper/@Axes/fillBetween.m
@@ -1,5 +1,31 @@
 function h = fillBetween(a, x1, y1, x2, y2, varargin )
 %AXES.FILLBETWEEN plot a filled area between the two series
+    narginOffset = 0;
+    if (isa(x1, 'AbstractTrace'))
+        narginOffset = 1;
+        if (nargin > 4)
+            varargin = [{y2}, varargin];
+        end
+        if (nargin > 3)
+            y2 = x2;
+        end
+        if (nargin > 2)
+            x2 = y1;
+        elseif (numel(x1) > 1)
+            narginOffset = 2;
+            x2 = x1(2);
+            x1 = x1(1);
+        end
+        y1 = x1.value;
+        x1 = x1.time;
+    end
+    if (isa(x2, 'AbstractTrace'))
+        if (nargin + narginOffset > 4)
+            varargin = [{y2}, varargin];
+        end
+        y2 = x2.value;
+        x2 = x2.time;
+    end
     f1 = isfinite(y1);
     x1 = x1(f1);
     y1=y1(f1);
