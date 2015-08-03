62b784eebfd56ee6a3bd46d8e2afb9c6075136ab
kkapsner <github@mail.kkapsner.de>
2015-08-03 18:37:29 +0200

    Added shift types at ShitedTrace

--- classes/Trace/@ShiftedTrace/ShiftedTrace.m
+++ classes/Trace/@ShiftedTrace/ShiftedTrace.m
@@ -51,22 +51,86 @@ classdef ShiftedTrace < TraceDecorator & handle
         end
         
         function setTimeShift(this, value)
-            if (numel(value) ~= 1)
-                error('ShiftedTrace:invalidTimeShift', ...
-                    'Time shift has to be scalar.');
-            end
-            for o = this
-                o.timeShift = value;
+            if (ischar(value))
+                switch value
+                    case 'first'
+                        for o = this
+                            o.timeShift = -o.trace.time(1);
+                        end
+                    case 'last'
+                        for o = this
+                            o.timeShift = -o.trace.time(end);
+                        end
+                    case 'min'
+                        for o = this
+                            o.timeShift = -min(o.trace.time);
+                        end
+                    case 'mean'
+                        for o = this
+                            o.timeShift = -mean(o.trace.time);
+                        end
+                    case 'median'
+                        for o = this
+                            o.timeShift = -median(o.trace.time);
+                        end
+                    case 'max'
+                        for o = this
+                            o.timeShift = -max(o.trace.time);
+                        end
+                    otherwise
+                        error('ShiftedTrace:unknownTimeShiftType', ...
+                            'Unknown time shift type.');
+                end
+            else
+                if (numel(value) ~= 1)
+                    error('ShiftedTrace:invalidTimeShift', ...
+                        'Time shift has to be scalar.');
+                end
+                for o = this
+                    o.timeShift = value;
+                end
             end
         end
         
         function setValueShift(this, value)
-            if (numel(value) ~= 1)
-                error('ShiftedTrace:invalidValueShift', ...
-                    'Value shift has to be scalar.');
-            end
-            for o = this
-                o.valueShift = value;
+            if (ischar(value))
+                switch value
+                    case 'first'
+                        for o = this
+                            o.valueShift = -o.trace.value(1);
+                        end
+                    case 'last'
+                        for o = this
+                            o.valueShift = -o.trace.value(end);
+                        end
+                    case 'min'
+                        for o = this
+                            o.valueShift = -min(o.trace.value);
+                        end
+                    case 'mean'
+                        for o = this
+                            o.valueShift = -mean(o.trace.value);
+                        end
+                    case 'median'
+                        for o = this
+                            o.valueShift = -median(o.trace.value);
+                        end
+                    case 'max'
+                        for o = this
+                            o.valueShift = -max(o.trace.value);
+                        end
+                    otherwise
+                        error('ShiftedTrace:unknownValueShiftType', ...
+                            'Unknown value shift type.');
+                end
+            else
+                if (numel(value) ~= 1)
+                    error('ShiftedTrace:invalidValueShift', ...
+                        'Value shift has to be scalar.');
+                end
+                for o = this
+                    o.valueShift = value;
+                end
             end
         end
     end
