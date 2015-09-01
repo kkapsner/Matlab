5f2928794ba750c1ae3414b47129fa6ed8c943d5
kkapsner <github@mail.kkapsner.de>
2015-09-01 17:52:16 +0200

    Significant performance improvement in get2DDatasetRegression.

--- functions/get2DDatasetRegression.m
+++ functions/get2DDatasetRegression.m
@@ -9,13 +9,13 @@ function [cx, cy, alpha, var1, var2] = get2DDatasetRegression(x, y, w)
 %       variance orthogonal to the line (VAR1) and parallel to the line
 %       (VAR2)
 
-    x = reshape(x, [], 1);
-    y = reshape(y, [], 1);
+    x = x(:);
+    y = y(:);
     
     if (nargin < 3)
         w = ones(size(x));
     else
-        w = reshape(w, [], 1);
+        w = w(:);
     end
     
     wSum = sum(w);
@@ -31,12 +31,18 @@ function [cx, cy, alpha, var1, var2] = get2DDatasetRegression(x, y, w)
     
     alpha = atan(2*C / (A - B)) / 2;
     
-    var1 = sin(alpha).^ 2 * A + cos(alpha) .^ 2 * B - 2 * sin(alpha) * cos(alpha) * C;
-    var2 = cos(alpha).^ 2 * A + sin(alpha) .^ 2 * B + 2 * sin(alpha) * cos(alpha) * C;
+    s = sin(alpha);
+    c = cos(alpha);
+    
+    var1 = s * s * A + c * (c * B - 2 * s * C);
+    var2 = A + B - var1;
+%     var2 = c * c * A + s * (s * B + 2 * c * C);
     
     if (var1 > var2)
         alpha = alpha + pi/2;
-        [var1, var2] = deal(var2, var1);
+        help = var1;
+        var1 = var2;
+        var2 = help;
     end
     
     var1 = var1 / wSum;
