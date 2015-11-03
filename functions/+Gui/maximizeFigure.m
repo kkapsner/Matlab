a009f538c8ace2a46ee91035ba423e24c80a8495
kkapsner <github@mail.kkapsner.de>
2015-11-03 17:46:46 +0100

    Workaround for NullPointer Exception in Gui.maximizeFigure

--- functions/+Gui/maximizeFigure.m
+++ functions/+Gui/maximizeFigure.m
@@ -1,15 +1,24 @@
 function maximizeFigure(fig)
 %MAXIMIZEFIGURE maximizes a figure to full screen
 %   Gui.maximizeFigure() maximizes the current figure
-%   Gui.macimizeFigure(f) maximizes figure f.
+%   Gui.maximizeFigure(f) maximizes figure f.
     
     if (nargin < 1)
         fig = gcf;
     end
     
-    warning('off','MATLAB:HandleGraphics:ObsoletedProperty:JavaFrame');
-    jf = get(fig, 'JavaFrame');
-    set(jf ,'maximized', 1);
-    warning('on','MATLAB:HandleGraphics:ObsoletedProperty:JavaFrame');
+    start(timer('TimerFcn', @setMaximized, 'StartDelay', 0.1, 'TasksToExecute', 1, 'StopFcn', @(t, ~)delete(t)));
+    
+    function setMaximized(~,~)
+        try
+            if (ishandle(fig))
+                warning('off','MATLAB:HandleGraphics:ObsoletedProperty:JavaFrame');
+                jf = get(fig, 'JavaFrame');
+                javaMethodEDT('setMaximized', jf, 1);
+                warning('on','MATLAB:HandleGraphics:ObsoletedProperty:JavaFrame');
+            end
+        catch
+            start(timer('TimerFcn', @setMaximized, 'StartDelay', 1, 'TasksToExecute', 1, 'StopFcn', @(t, ~)delete(t)));
+        end
+    end
 end
-
